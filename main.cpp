2a425b2ff51ade5dbf91955d150aac22d81022f5
Superale0102 <superalex0102@gmail.com>
2025-02-18 23:50:38 +0100

    Added basic CMake config

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
+#include <yaml-cpp/yaml.h>
+
+const std::string INPUT_FILE = "../config/params_input_file.yaml";
 
 int main() {
-
+    YAML::Node config = YAML::LoadFile(INPUT_FILE);
 }
