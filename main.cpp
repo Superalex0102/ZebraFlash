2b2dcd68be81a548f65fcca82610e584645a3d1d
Superale0102 <superalex0102@gmail.com>
2025-03-05 21:38:35 +0100

    Implemented some of the loop part that breaks down the frame.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <opencv2/opencv.hpp>
 #include <yaml-cpp/yaml.h>
-#include <numeric>
 
 const std::string INPUT_FILE = "..\\config\\params_input_file.yml";
 const std::string WINDOW_NAME = "window";
+
+float calculateMode(const cv::Mat& mat) {
+    std::unordered_map<float, int> frequency;
+
+    std::vector<float> vec;
+    mat.reshape(1, 1).copyTo(vec);
+
+    for (float v : vec) {
+        if (v != 0.0f) {
+            frequency[v]++;
+        }
+    }
+
+    auto mode = std::max_element(frequency.begin(), frequency.end(),
+        [](const auto& a, const auto& b) {
+            return a.second < b.second;
+        });
+
+    return (mode != frequency.end()) ? mode->first : 0.0f;
+}
 
 int main() {
     YAML::Node config = YAML::LoadFile(INPUT_FILE);
@@ -28,6 +47,8 @@ int main() {
     int iterations = config["iterations"].as<int>();
     int poly_n = config["poly_n"].as<int>();
     double poly_sigma = config["poly_sigma"].as<double>();
+
+    std::vector<std::vector<int>> directions_map(size, std::vector<int>(4, 0));
 
     cv::namedWindow(WINDOW_NAME, cv::WINDOW_NORMAL);
 
@@ -71,6 +92,73 @@ int main() {
         cv::Mat orig_frame = frame;
         frame = frame(cv::Range(mask_x_min, mask_x_max), cv::Range(mask_y_min, mask_y_max));
         cv::resize(frame, frame_resized, res, 0, 0, cv::INTER_CUBIC);
+
+        cv::Mat gray;
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+
+        cv::Mat flow;
+        cv::calcOpticalFlowFarneback(gray_previous, gray, flow, pyr_scale, levels,
+            winsize, iterations, poly_n, poly_sigma, cv::OPTFLOW_LK_GET_MIN_EIGENVALS);
+
+        std::vector<cv::Mat> flow_channels(2);
+        cv::split(flow, flow_channels);
+
+        cv::Mat mag, ang;
+
+        cv::cartToPolar(flow_channels[0], flow_channels[1], mag, ang, true);
+
+        cv::Mat ang_180 = ang / 2;
+
+        gray_previous = gray;
+
+        cv::Mat mask = mag > threshold;
+
+        cv::Mat move_sense;
+        ang.copyTo(move_sense, mask);
+
+        int angle_offset = 0;
+        bool is_moving_up = std::any_of(move_sense.begin<>, move_sense.end<>,
+            [angle_min, angle_max](float m) {
+                return (m >= angle_min && m <= angle_max);
+            });
+
+        float move_mode = calculateMode(move_sense);
+
+        if (is_moving_up) {
+            directions_map[directions_map.size() - 1][0] = 3.5f;
+            directions_map[directions_map.size() - 1][1] = 0;
+            directions_map[directions_map.size() - 1][2] = 0;
+            directions_map[directions_map.size() - 1][3] = 0;
+        }
+        else if (move_mode < angle_min || angle_max < move_mode) {
+            directions_map[directions_map.size() - 1][0] = 0;
+            directions_map[directions_map.size() - 1][1] = 1;
+            directions_map[directions_map.size() - 1][2] = 0;
+            directions_map[directions_map.size() - 1][3] = 0;
+        }
+        else { //No movement detected
+            cv::Mat fg_mask;
+            backSub->apply(frame, fg_mask);
+
+            cv::Mat fg_mask_blurred;
+            cv::GaussianBlur(fg_mask, fg_mask_blurred, cv::Size(7, 7), 0);
+
+            cv::Mat tresh_frame;
+            cv::threshold(fg_mask_blurred, tresh_frame, binary_threshold, 255, cv::THRESH_BINARY);
+
+            if (cv::countNonZero(tresh_frame) > threshold_count) {
+                directions_map[directions_map.size() - 1][0] = 0;
+                directions_map[directions_map.size() - 1][1] = 0;
+                directions_map[directions_map.size() - 1][2] = 1;
+                directions_map[directions_map.size() - 1][3] = 0;
+            }
+            else { //No movement, no difference detected
+                directions_map[directions_map.size() - 1][0] = 0;
+                directions_map[directions_map.size() - 1][1] = 0;
+                directions_map[directions_map.size() - 1][2] = 0;
+                directions_map[directions_map.size() - 1][3] = 1;
+            }
+        }
 
         cv::imshow(WINDOW_NAME, frame);
 
