a1abcb642b9170017f864a0f439c07cb6a90019f
Superale0102 <superalex0102@gmail.com>
2025-06-05 22:02:17 +0200

    Removed debug message

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@
 const std::string INPUT_FILE = "../../config/params_input_file.yml";
 
 int main() {
-    std::cout << "Current working directory: "
-              << std::filesystem::current_path() << std::endl;
     try {
         MotionDetector detector(INPUT_FILE);
         detector.run();
